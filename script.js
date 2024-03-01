8939bb262736808cf52dd3a186e0cf553e7e526a
JuanJoseBastoGonzalez <151982381+JuanJoseBastoGonzalez@users.noreply.github.com.>
2024-03-01 17:24:32 -0500

    feat: :art: maquetacion basica
    maquetacion basica

---
+++ script.js
@@ -0,0 +1,9 @@
+     // Función para abrir la ventana de diálogo
+     function openDialog() {
+        document.getElementById('dialog').style.display = 'flex';
+    }
+
+    // Función para cerrar la ventana de diálogo
+    function closeDialog() {
+        document.getElementById('dialog').style.display = 'none';
+    }
