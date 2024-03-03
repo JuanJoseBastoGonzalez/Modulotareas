82027b037f80d8b29a57befdea4f3c0cb9b3f904
JuanJoseBastoGonzalez <151982381+JuanJoseBastoGonzalez@users.noreply.github.com.>
2024-03-03 11:20:18 -0500

    feat:

--- script.js
+++ script.js
@@ -1,75 +1,50 @@
-// var dataUp = [];
-// var dataDown =[];
-// var butonP = document.getElementById("priorityUp");
-// butonP.addEventListener("click", () => {
-//     var dataDic = {
-//         "nombre": "",
-//         "dateStart": "",
-//         "dateFinish": "",
-//         "prioridad":"Alta"
-//     };
-//     var nombreT = document.getElementById("task").value;
-//     var fechaInicio = document.getElementById("date1").value;
-//     var fechaFin = document.getElementById("date2").value;
-//     dataDic["nombre"] = nombreT;
-//     dataDic["dateStart"] = fechaInicio;
-//     dataDic["dateFinish"] = fechaFin;
-//     dataUp.push(dataDic);
-//     alert(JSON.stringify(dataDic));
-//     alert(JSON.stringify(data));
-//     console.log(data)
-// });
-// dataUp.forEach(Element=>{
-//     const tareasp = document.getElementById("lista")
-//     tareasp.innerHTML+=
-//     g
 
-// });
 
-// URL del servidor JSON
-const url = 'http://localhost:3000/prioridad'; // Asegúrate de que la URL sea correcta
-
-// Realizar una solicitud GET para obtener los datos actuales del servidor
-// fetch(url)
-//   .then(response => {
-//     if (!response.ok) {
-//       throw new Error('Error al obtener los datos del servidor');
-//     }
-//     return response.json();
-//   })
-//   .then(data => {
-//     // Agregar el nuevo objeto al array "alta"
-//     data.alta.push({ 
-//       // Aquí puedes definir los campos de tu nuevo objeto
-//       campo1: "valor1",
-//       campo2: "valor2,
-//       // ...
-//     });
-
-//     // Configuración de la solicitud PUT para enviar los datos actualizados al servidor
-//     const opciones = {
-//       method: 'PUT',
-//       headers: {
-//         'Content-Type': 'application/json'
-//       },
-//       body: JSON.stringify(data)
-//     };
-
-//     // Enviar la solicitud PUT al servidor
-//     return fetch(url, opciones);
-//   })
-//   .then(response => {
-//     if (!response.ok) {
-//       throw new Error('Error al enviar los datos actualizados al servidor');
-//     }
-//     console.log('Objeto agregado exitosamente al array "alta" en el servidor');
-//   })
-
+document.getElementById('priorityUp').addEventListener('click', function() {
+  const url = 'http://localhost:3000/prioridad';
+  var nombreT = document.getElementById("task").value;
+  var fechaInicio = document.getElementById("date1").value;
+  var fechaFin = document.getElementById("date2").value;
+  fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Error al obtener los datos del servidor');
+      }
+      return response.json();
+    })
+    .then(data => {
+      data.alta.push({ 
+        nombre: nombreT,
+        dateStart: fechaInicio,
+        dateFinish: fechaFin,
+        prioridad:"Alta"
+      });
+      const opciones = {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      };
+      return fetch(url, opciones);
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Error al enviar los datos actualizados al servidor');
+      }
+      console.log('Objeto agregado exitosamente al array "alta" en el servidor');
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+});
 
 
 document.getElementById('priorityDown').addEventListener('click', function() {
     const url = 'http://localhost:3000/prioridad';
-  
+    var nombreT = document.getElementById("task").value;
+    var fechaInicio = document.getElementById("date1").value;
+    var fechaFin = document.getElementById("date2").value;
     fetch(url)
       .then(response => {
         if (!response.ok) {
@@ -78,13 +53,12 @@ document.getElementById('priorityDown').addEventListener('click', function() {
         return response.json();
       })
       .then(data => {
-        data.alta.push({ 
-          // Aquí puedes definir los campos de tu nuevo objeto
-          campo1: "valor1",
-          campo2: "valor2",
-          // ...
+        data.baja.push({ 
+          nombre: nombreT,
+          dateStart: fechaInicio,
+          dateFinish: fechaFin,
+          prioridad:"Baja"
         });
-  
         const opciones = {
           method: 'PUT',
           headers: {
@@ -92,7 +66,6 @@ document.getElementById('priorityDown').addEventListener('click', function() {
           },
           body: JSON.stringify(data)
         };
-  
         return fetch(url, opciones);
       })
       .then(response => {
@@ -105,7 +78,36 @@ document.getElementById('priorityDown').addEventListener('click', function() {
         console.error('Error:', error);
       });
   });
-  
+  const url = 'http://localhost:3000/prioridad'
+// dataUp.forEach(Element=>{
+//     const tareasp = document.getElementById("lista")
+//     tareasp.innerHTML+=
+//     g
+
+// });
+url.alta.forEach(element => {
+    const url = 'http://localhost:3000/prioridad';
+    
+    const tareasp = document.getElementById("lista");
+    const addCodeTask =document.getElementById("lista")
+    addCodeTask.innerHTML+= `
+    <section class="titleTask">${Element.nombre}</section>
+    <section class="dateTaskI">${element.}</section>
+    <section class="dateTaskF">1</section>
+    <section >
+         <button class="chekY" id="chekY"><i class="fa-regular fa-circle-check" style="color: #000000;"></i></button>
+         <button class="chekN" id="chekN" ><i class="fa-regular fa-circle-xmark" style="color: #000000;"></i></button>
+         
+    </section>
+    
+    `;
+    data.alta
+
+});
+
+
+
+
 
 
 
